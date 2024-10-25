fdbd3fbfecf847598881f7a70ff7c111e286c22c
walther <waltherpires@hotmail.com>
2024-10-25 01:10:57 -0300

    correcao erro ao acessar service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BackendBiblioteca.Data;
+using BackendBiblioteca.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
